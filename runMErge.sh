43c87633531f7ffa9d81a450187fb5f01e7e2564
Abdallah Cheikh <38982756+AESASH@users.noreply.github.com>
2018-07-06 15:40:08 +0200

    Update runMErge.sh

--- runMErge.sh
+++ runMErge.sh
@@ -44,20 +44,34 @@ then
 	# COPY VHDL FILES
 	cp -r $KLESS_PATH/klessydra-t1-3th $PULP_PATH/ips
 
-	# COPY CMAKE SHELL SCRIPTS
-	cp $KLESS_PATH/patched_files/t13x_patched_files/cmake_configure.klessydra-t1-3th.gcc.sh $PULP_PATH/sw
-	cp $KLESS_PATH/patched_files/common_patched_files/cmake_configure.microriscy.gcc.sh $PULP_PATH/sw
-	cp $KLESS_PATH/patched_files/common_patched_files/cmake_configure.riscv.gcc.sh $PULP_PATH/sw
-	cp $KLESS_PATH/patched_files/common_patched_files/cmake_configure.riscvfloat.gcc.sh $PULP_PATH/sw
-	cp $KLESS_PATH/patched_files/common_patched_files/cmake_configure.zeroriscy.gcc.sh $PULP_PATH/sw
+	# COPY CMAKE/SHELL SCRIPTS
+	cp -f $KLESS_PATH/patched_files/t13x_patched_files/cmake_configure.klessydra-t1-3th.gcc.sh $PULP_PATH/sw
+	cp -f $KLESS_PATH/patched_files/common_patched_files/cmake_configure.microriscy.gcc.sh $PULP_PATH/sw
+	cp -f $KLESS_PATH/patched_files/common_patched_files/cmake_configure.riscv.gcc.sh $PULP_PATH/sw
+	cp -f $KLESS_PATH/patched_files/common_patched_files/cmake_configure.riscvfloat.gcc.sh $PULP_PATH/sw
+	cp -f $KLESS_PATH/patched_files/common_patched_files/cmake_configure.zeroriscy.gcc.sh $PULP_PATH/sw
 
 	# COPY CMAKE FILES
 	cp -f $KLESS_PATH/patched_files/common_patched_files/CMakeLists.txt $PULP_PATH/sw
-	cp -f $KLESS_PATH/patched_files/common_patched_files/CMakeSim.txt $PULP_PATH/sw/apps
+	cp -f $KLESS_PATH/patched_files/common_patched_files/apps/CMakeSim.txt $PULP_PATH/sw/apps
+        cp -f $KLESS_PATH/patched_files/common_patched_files/apps/CMakeLists.txt $PULP_PATH/sw/apps
 
 	# COPY STARTUP FILES
-	cp $KLESS_PATH/patched_files/common_patched_files/crt0.klessydra.S $PULP_PATH/sw/ref
-	cp $KLESS_PATH/patched_files/common_patched_files/klessydra.h $PULP_PATH/sw/libs/sys_lib/inc
+	cp -f $KLESS_PATH/patched_files/common_patched_files/ref/crt0.klessydra.S $PULP_PATH/sw/ref
+
+	# COPY KLESSYDRA APPS 
+	cp -r $KLESS_PATH/patched_files/common_patched_files/apps/klessydra_tests $PULP_PATH/sw/apps
+
+        # COPY KLESSYDRA LIBRARIES AND HEADERS
+        cp -r $KLESS_PATH/patched_files/common_patched_files/klessydra_lib $PULP_PATH/sw/libs/
+
+	# COPY PATCHED LIBS
+        cp -f $KLESS_PATH/patched_files/common_patched_files/sys_lib/inc/cpu_hal.h $PULP_PATH/sw/libs/sys_lib/inc
+        cp -f $KLESS_PATH/patched_files/common_patched_files/sys_lib/inc/int.h $PULP_PATH/sw/libs/sys_lib/inc
+        cp -f $KLESS_PATH/patched_files/common_patched_files/sys_lib/inc/utils.h $PULP_PATH/sw/libs/sys_lib/inc
+	cp -f $KLESS_PATH/patched_files/common_patched_files/sys_lib/inc/klessydra.h $PULP_PATH/sw/libs/sys_lib/inc
+        cp -f $KLESS_PATH/patched_files/common_patched_files/bench_lib/src/bench.c $PULP_PATH/sw/libs/bench_lib/src
+        cp -f $KLESS_PATH/patched_files/common_patched_files/sys_lib/src/utils.c $PULP_PATH/sw/libs/sys_lib/src
 
 	# COPY COMPILE SCRIPTS
 	cp $KLESS_PATH/patched_files/t13x_patched_files/vcompile_klessydra-t1-3th.csh $PULP_PATH/vsim/vcompile
